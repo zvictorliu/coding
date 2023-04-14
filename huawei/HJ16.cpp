73d67cab56e077e0096f1224a6ac502f6753f639
zvictorliu <zvictorliu@outlook.com>
2023-04-14 22:23:16 +0800

    HJ16

--- huawei/HJ16.cpp
+++ huawei/HJ16.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class Solution{
+
+public:
+    int max_satification(int N, vector<vector<int>> items, int num){
+        vector<vector<int>> dp(num, vector<int> (N+1,0)); // 是可以取到N的
+
+        for (int i = 0; i < num; i++){
+            for (int j = 0; j <= N; j++){
+                // dp[i][j] 是 不要该主件 与 要主件（只要主件，主件+附件1，主件+附件2，主件+附件1和2）中最优的结果
+
+
+            }
+
+        }
+
+    }
+
+};
 
 int main() {
     
-    int N, m;
+    int N, m, num = 0;
     cin >> N >> m;
-
-    char arr[60][3]; // 3行60列
-
-    for (int i = 0; i < m; i++){
-        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
-        // 价格行，权重行，属性行，同时i+1也是编号
+    vector<vector<vector<int>>> data(m+1, vector<vector<int>> (2, {0}));
+    for (int i = 1; i <= m; i++){
+        int p;
+        vector<int> it(2,0);
+        cin >> it[0] >> it[1] >> p;
+        if (p == 0){
+            data[i][0][0] = it[0];
+            data[i][1][0] = it[1];
+        }
+        else {
+            data[p].push_back(it);
+        }
     }
 
-    // 太tm多东西了
-    
+    Solution res;
+    // cout << res.max_satification(N, items, num) << endl;
+
 
     return 0;
 }
