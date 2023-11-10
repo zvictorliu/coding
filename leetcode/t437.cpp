60da252c1e4ecc1085ca48da159f46fbc8663992
zvictorliu <zvictorliu@outlook.com>
2023-11-10 21:43:15 +0800

    add 437

--- leetcode/t437.cpp
+++ leetcode/t437.cpp
@@ -18,13 +18,37 @@ struct TreeNode {
 };
 
 /*
-    一种情况很多的感觉
-    做不太出来
+    前序遍历可以计算出每个节点的累计值
+    但是依然不能解决 （暴力点，每个节点都前序遍历
+    前缀和解决重复计算
+    又用到了哈希表
 */
 class Solution {
+    unordered_map<long,int> umap; // 前缀和-次数
+    int target;
 public:
     int pathSum(TreeNode* root, int targetSum) {
+        umap[0] = 1;
+        target = targetSum;
+        return dfs(root, 0);
+    }
 
+    int dfs(TreeNode* root, long curr){
+        int cnt = 0;
+        if (root == nullptr)
+            return 0;
+        curr += root->val;
+        // 检测前面是否有合适的节点
+        if (umap.count(curr - target)){ //说明存在
+            cnt += umap[curr-target];
+        }
+        umap[curr] ++; //! 应该放在后面，不能把自己也算进去了
+        cnt += dfs(root->left, curr);
+        cnt += dfs(root->right, curr);
+        //! 退回来应该不计入了,否则会引起重复
+        umap[curr] --;
+
+        return cnt;
     }
 };
 
